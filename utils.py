b9f9cad7621d064fe2b1f31d48ae50185e23b34c
Younes Kazemi <youneskazemi9798@gmail.com>
2025-09-07 21:15:39 +0330

    fix file name

--- utils.py
+++ utils.py
@@ -12,14 +12,16 @@ import world
 class BPRLoss:
     def __init__(self, recmodel, config):
         self.model = recmodel
-        self.weight_decay = config['decay']  # 1e-4
-        self.lr = config['lr']
+        self.weight_decay = config["decay"]  # 1e-4
+        self.lr = config["lr"]
         self.opt = optim.Adam(recmodel.parameters(), lr=self.lr)
 
     def stageOne(self, users, pos, neg, epoch):
         # loss, reg_loss = self.model.bpr_loss(users, pos, neg)  # loss, E0^2
 
-        loss, reg_loss, attr_loss, lbl_loss, tag = self.model.bpr_loss(users, pos, neg, epoch)  # loss, E0^2
+        loss, reg_loss, attr_loss, lbl_loss, tag = self.model.bpr_loss(
+            users, pos, neg, epoch
+        )  # loss, E0^2
         attr_loss = attr_loss * 9
         reg_loss = reg_loss * self.weight_decay
         loss = loss + reg_loss
@@ -41,8 +43,8 @@ def UniformSample_original(users, dataset):
     users = np.random.randint(0, dataset.n_users, user_num)
     allPos = dataset.allPos
     S = []
-    sample_time1 = 0.
-    sample_time2 = 0.
+    sample_time1 = 0.0
+    sample_time2 = 0.0
     for i, user in enumerate(users):
         start = time()
         posForUser = allPos[user]
@@ -67,6 +69,7 @@ def UniformSample_original(users, dataset):
 # ===================end samplers==========================
 # =====================utils====================================
 
+
 def set_seed(seed):
     np.random.seed(seed)
     if torch.cuda.is_available():
@@ -76,32 +79,34 @@ def set_seed(seed):
 
 
 def getFileName():
-    if world.model_name == 'bpr':
+    print(world.model_name)
+    if world.model_name == "bpr":
         file = f"mf-{world.dataset}-{world.config['latent_dim_rec']}.pth.tar"
-    elif world.model_name in ['LightGCN', 'CLDS']:
-        file = f"{world.model_name}-{world.dataset}-{world.config['layer']}layer-" \
-               f"{world.config['latent_dim_rec']}.pth.tar"
+    elif world.model_name in ["LightGCN", "CLDS", "TCLDS"]:
+        file = (
+            f"{world.model_name}-{world.dataset}-{world.config['layer']}layer-"
+            f"{world.config['latent_dim_rec']}.pth.tar"
+        )
     return os.path.join(world.FILE_PATH, file)
 
 
 def minibatch(*tensors, **kwargs):
-    batch_size = kwargs.get('batch_size', world.config['bpr_batch_size'])
+    batch_size = kwargs.get("batch_size", world.config["bpr_batch_size"])
 
     if len(tensors) == 1:
         tensor = tensors[0]
         for i in range(0, len(tensor), batch_size):
-            yield tensor[i:i + batch_size]
+            yield tensor[i : i + batch_size]
     else:
         for i in range(0, len(tensors[0]), batch_size):
-            yield tuple(x[i:i + batch_size] for x in tensors)
+            yield tuple(x[i : i + batch_size] for x in tensors)
 
 
 def shuffle(*arrays, **kwargs):
-    require_indices = kwargs.get('indices', False)
+    require_indices = kwargs.get("indices", False)
 
     if len(set(len(x) for x in arrays)) != 1:
-        raise ValueError('All inputs to shuffle must have '
-                         'the same length.')
+        raise ValueError("All inputs to shuffle must have " "the same length.")
 
     shuffle_indices = np.arange(len(arrays[0]))
     np.random.shuffle(shuffle_indices)
@@ -125,12 +130,12 @@ def RecallPrecision_ATk(test_data, r, k):
     recall_n = np.array([len(test_data[i]) for i in range(len(test_data))])
     recall = np.sum(right_pred / recall_n)
     precis = np.sum(right_pred) / precis_n
-    return {'recall': recall, 'precision': precis}
+    return {"recall": recall, "precision": precis}
 
 
 def MRRatK_r(r, k):
     pred_data = r[:, :k]
-    scores = np.log2(1. / np.arange(1, k + 1))
+    scores = np.log2(1.0 / np.arange(1, k + 1))
     pred_data = pred_data / scores
     pred_data = pred_data.sum(1)
     return np.sum(pred_data)
@@ -145,12 +150,12 @@ def NDCGatK_r(test_data, r, k):
         length = k if k <= len(items) else len(items)
         test_matrix[i, :length] = 1
     max_r = test_matrix
-    idcg = np.sum(max_r * 1. / np.log2(np.arange(2, k + 2)), axis=1)
-    dcg = pred_data * (1. / np.log2(np.arange(2, k + 2)))
+    idcg = np.sum(max_r * 1.0 / np.log2(np.arange(2, k + 2)), axis=1)
+    dcg = pred_data * (1.0 / np.log2(np.arange(2, k + 2)))
     dcg = np.sum(dcg, axis=1)
-    idcg[idcg == 0.] = 1.
+    idcg[idcg == 0.0] = 1.0
     ndcg = dcg / idcg
-    ndcg[np.isnan(ndcg)] = 0.
+    ndcg[np.isnan(ndcg)] = 0.0
     return np.sum(ndcg)
 
 
@@ -170,7 +175,8 @@ def getLabel(test_data, pred_data):
         pred = list(map(lambda x: x in groundTrue, predictTopK))
         pred = np.array(pred).astype("float")
         r.append(pred)
-    return np.array(r).astype('float')
+    return np.array(r).astype("float")
+
 
 # ====================end Metrics=============================
 # =========================================================
